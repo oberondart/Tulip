94f73083ac854d92d0ac166c8606a7ed68a81d09
devenoch <durustein@gmail.com>
2026-02-02 15:45:49 -0500

    Update standardlib.lua

--- standardlib.lua
+++ standardlib.lua
@@ -8,4 +8,20 @@ function standard_functions.wait(ms)
     os.execute("sleep " .. tonumber(ms) / 1000)
 end
 
+function standard_functions.add(x, y)
+    return x + y
+end
+
+function standard_functions.sub(x, y)
+    return x - y
+end
+
+function standard_functions.mul(x, y)
+    return x * y
+end
+
+function standard_functions.div(x, y)
+    return x / y
+end
+
 return standard_functions
